4e50f6d92c71bb62b3b0bb5b4b0cecf36078e2b3
equirogag <equiroga@addval.cl>
2023-01-31 14:26:40 -0300

    Para campo gerencia ahora se hace un manetedor, primer commit

---
+++ purchase_approval_route/models/purchase_approval_management.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError
+
+
+class PurchaseApprovalManagement(models.Model):
+    _name = "purchase.approval.management"
+    _description = "Gerencia para las aprobaciones de ordenes de compra"
+
+    name = fields.Char('Nombre gerente')
+
+    filter_for_approve  = fields.Char(string='Filtro a usar en el flujo', 
+        help="Se recomienda usar el siguiente formato: NOMBRE_APELLIDO, de esta manera se evitan los problemas en los filtros por espacios de más")
+
+    company_id = fields.Many2one('res.company', string='Company', readonly=True, required=True,
+        default=lambda self: self.env.company)
