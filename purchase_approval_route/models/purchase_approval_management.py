9eafe564633ab850b1f93e5f03bbd66f94e4d040
equirogag <equiroga@addval.cl>
2023-02-09 13:39:39 -0300

    campo computado para filtro gerencia

--- purchase_approval_route/models/purchase_approval_management.py
+++ purchase_approval_route/models/purchase_approval_management.py
@@ -10,7 +10,17 @@ class PurchaseApprovalManagement(models.Model):
     name = fields.Char('Nombre gerente')
 
     approve_filter  = fields.Char(string='Filtro a usar en el flujo', 
+        compute="_compute_filter",
         help="Se recomienda usar el siguiente formato: NOMBRE_APELLIDO, de esta manera se evitan los problemas en los filtros por espacios de más")
 
     company_id = fields.Many2one('res.company', string='Company', required=True,
         default=lambda self: self.env.company)
+
+    @api.depends('name')
+    def _compute_filter(self):
+
+        upper_name = self.name.upper()
+
+        replace_space = upper_name.replace(" ","_")
+
+        self.approve_filter = replace_space
