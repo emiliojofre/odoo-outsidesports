e165a8675a8c8d5ed93170e3ffc2e341f1403395
equirogag <equiroga@addval.cl>
2023-02-09 10:19:01 -0300

    Cambio en campo compañia de gerencia a no solo lectura

--- purchase_approval_route/models/purchase_approval_management.py
+++ purchase_approval_route/models/purchase_approval_management.py
@@ -12,5 +12,5 @@ class PurchaseApprovalManagement(models.Model):
     approve_filter  = fields.Char(string='Filtro a usar en el flujo', 
         help="Se recomienda usar el siguiente formato: NOMBRE_APELLIDO, de esta manera se evitan los problemas en los filtros por espacios de más")
 
-    company_id = fields.Many2one('res.company', string='Company', readonly=True, required=True,
+    company_id = fields.Many2one('res.company', string='Company', required=True,
         default=lambda self: self.env.company)
