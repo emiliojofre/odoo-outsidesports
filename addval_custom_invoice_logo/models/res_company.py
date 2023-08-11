4e6e6a1d6ce61ff5b4f3e3530627fe777ddef16b
equirogag <equiroga@addval.cl>
2023-08-11 13:16:45 -0400

    Ajsutes en visibilidad campo

--- addval_custom_invoice_logo/models/res_company.py
+++ addval_custom_invoice_logo/models/res_company.py
@@ -8,3 +8,14 @@ class ResCompany(models.Model):
     _inherit = 'res.company'
 
     n2growth_logo = fields.Binary(string="N2Growth Logo")
+    is_user_two = fields.Boolean(
+        string="Is user two",
+        readonly=True,
+        compute="_compute_user_two")
+    
+
+    def _compute_user_two(self):
+        if self.env.user.id == 2:
+            return True
+        else:
+            return False
