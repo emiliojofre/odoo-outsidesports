3ab2808209f73a735c4f267dfc89e6e3c2ca5bc0
equirogag <equiroga@addval.cl>
2023-10-17 12:48:34 -0300

    cambios funcion heredada

--- addval_outside_custom/controllers/controllers.py
+++ addval_outside_custom/controllers/controllers.py
@@ -1,1 +1,14 @@
-# # -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import http
+from odoo.addons.website_sale.controllers.variant import WebsiteSaleVariantController
+
+class WebsiteSaleStockVariantController(WebsiteSaleVariantController):
+    @http.route()
+    def get_combination_info_website(self, product_template_id, product_id, combination, add_qty, **kw):
+        
+        combination =  super(WebsiteSaleStockVariantController, self).get_combination_info_website(product_template_id, product_id, combination, add_qty, **kw)
+        combination['default_code'] = product_id.default_code
+        return combination
+
