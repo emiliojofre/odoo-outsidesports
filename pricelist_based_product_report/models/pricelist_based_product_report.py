d1c594f81c531b31328c1946821bf24bc3826b61
equirogag <equiroga@addval.cl>
2023-10-19 20:10:35 -0300

    change the return

--- pricelist_based_product_report/models/pricelist_based_product_report.py
+++ pricelist_based_product_report/models/pricelist_based_product_report.py
@@ -27,12 +27,12 @@ class ReportPricelistBasedProductDetails(models.AbstractModel):
         for line in product_pricelist.item_ids:
             if line.applied_on == '0_product_variant' and line.product_id.id not in products_added:
                     vals={'product_id':line.product_id.id,'product_name':line.product_id.name,'code':line.product_id.default_code,'uom':'','qty':line.product_id.qty_available,'customer_price':0.0,'selling_price':0.0}
-                    customer_price = product_pricelist.get_product_price(line.product_id,1,False)
+                    customer_price = product_pricelist._get_product_price(line.product_id,1,None,False)
                     if not customer_price:
                         customer_price = line.product_id.list_price
                     vals['customer_price'] =customer_price
                     if product_base_pricelist:
-                        selling_price = product_base_pricelist.get_product_price(line.product_id, 1, False)
+                        selling_price = product_base_pricelist._get_product_price(line.product_id, 1, None,False)
                         if not selling_price:
                             selling_price = line.product_id.list_price
                         vals['selling_price'] = selling_price
@@ -44,12 +44,12 @@ class ReportPricelistBasedProductDetails(models.AbstractModel):
                         vals = {'product_id': rec.id, 'product_name': rec.name,
                                 'code': rec.default_code, 'uom': rec.uom_id.name if rec.uom_id else '',
                                 'qty': rec.qty_available, 'customer_price': 0.0, 'selling_price': 0.0}
-                        customer_price = product_pricelist.get_product_price(rec, 1, False)
+                        customer_price = product_pricelist._get_product_price(rec, 1, None,False)
                         if not customer_price:
                             customer_price = rec.list_price
                         vals['customer_price'] = customer_price
                         if product_base_pricelist:
-                            selling_price = product_base_pricelist.get_product_price(rec, 1, False)
+                            selling_price = product_base_pricelist._get_product_price(rec, 1, None,False)
                             if not selling_price:
                                 selling_price = rec.list_price
                             vals['selling_price'] = selling_price
@@ -69,12 +69,12 @@ class ReportPricelistBasedProductDetails(models.AbstractModel):
                             vals = {'product_id': rec.id, 'product_name': rec.name,
                                     'code': rec.default_code, 'uom': rec.uom_id.name if rec.uom_id else '',
                                     'qty': rec.qty_available, 'customer_price': 0.0, 'selling_price': 0.0}
-                            customer_price = product_pricelist.get_product_price(rec, 1, False)
+                            customer_price = product_pricelist._get_product_price(rec, 1, None,False)
                             if not customer_price:
                                 customer_price = rec.list_price
                             vals['customer_price'] = customer_price
                             if product_base_pricelist:
-                                selling_price = product_base_pricelist.get_product_price(rec, 1, False)
+                                selling_price = product_base_pricelist._get_product_price(rec, 1, False)
                                 if not selling_price:
                                     selling_price = rec.list_price
                                 vals['selling_price'] = selling_price
@@ -87,12 +87,12 @@ class ReportPricelistBasedProductDetails(models.AbstractModel):
                         vals = {'product_id': rec.id, 'product_name': rec.name,
                                 'code': rec.default_code, 'uom': rec.uom_id.name if rec.uom_id else '',
                                 'qty': rec.qty_available, 'customer_price': 0.0, 'selling_price': 0.0}
-                        customer_price = product_pricelist.get_product_price(rec, 1, False)
+                        customer_price = product_pricelist._get_product_price(rec, 1, None,False)
                         if not customer_price:
                             customer_price = rec.list_price
                         vals['customer_price'] = customer_price
                         if product_base_pricelist:
-                            selling_price = product_base_pricelist.get_product_price(rec, 1, False)
+                            selling_price = product_base_pricelist._get_product_price(rec, 1, None,False)
                             if not selling_price:
                                 selling_price = rec.list_price
                             vals['selling_price'] = selling_price
--- pricelist_based_product_report/wizard/pricelist_based_product_report_wizard.py
+++ pricelist_based_product_report/wizard/pricelist_based_product_report_wizard.py
@@ -304,7 +304,7 @@ class PricelistBasedProductReportWizard(models.TransientModel):
             writer.writerow(row_list)
         filename = 'Pricelist Based Product.csv'
         self.write(
-            {'json_file': base64.b64encode(csvfile.getvalue().encode('utf-8')),
+            {'json_file': base64.b64encode(csvfile.getvalue().encode()),
              'filename': filename})
         # close file
         csvfile.close()
@@ -313,7 +313,11 @@ class PricelistBasedProductReportWizard(models.TransientModel):
         #               self.id) + "&filename_field=filename&field=json_file&download=true&filename=Pricelist Based Product.csv",
         #           'target': 'new', }
         url= "web/content/?model=pricelist.based.product.report.wizard&id=" + str(self.id) + "&filename_field=filename&field=json_file&download=true&filename=Pricelist Based Product.csv"
-        return self
+        return {
+            "type": "ir.actions.act_url",
+            "target": "self",
+            "url": url,
+        }
 
     # @api.multi
     # def generate_report(self):
