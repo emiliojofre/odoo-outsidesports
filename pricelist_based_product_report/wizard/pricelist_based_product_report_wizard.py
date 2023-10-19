d29f33882cd242c771912cf47927937a087de09b
equirogag <equiroga@addval.cl>
2023-10-19 18:25:25 -0300

    b64 encode

--- pricelist_based_product_report/wizard/pricelist_based_product_report_wizard.py
+++ pricelist_based_product_report/wizard/pricelist_based_product_report_wizard.py
@@ -148,7 +148,7 @@ class PricelistBasedProductReportWizard(models.TransientModel):
             writer.writerow(row_list)
         filename = 'Descarga catálogo y stock.csv'
         self.write(
-            {'json_file': base64.encodestring(csvfile.getvalue().encode('ISO-8859-1')),
+            {'json_file': base64.b64encode(csvfile.getvalue().encode('ISO-8859-1')),
              'filename': filename})
         # close file
         csvfile.close()
