0e749b1a58fcdf1b3d2ce6419d80332af32f7cab
equirogag <equiroga@addval.cl>
2023-10-25 11:09:27 -0300

    primera prueba campos nuevos

---
+++ addval_custom_analytics/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+{
+   'name': "Customización de plan analitico ",
+
+    'summary': """
+        Se añaden dos campos nuevos relacionasdos al plan analítco""",
+
+    'description': """
+        """,
+
+    "author": "Addval Connect",
+    "website": "http://www.addval.cl",
+    "category": "Account",
+    "license": "Other proprietary",
+    'version': '0.1',
+
+    'depends': [
+        'base',
+        'account',
+        'sale',
+        'account_accountant'
+    ],
+
+    'data': [
+        #'security/ir.model.access.csv',
+        'views/res_company.xml',
+    ],
+
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
---
+++ addval_custom_analytics/models/account_move_line.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from odoo import _, api, fields, models
+import logging
+_logger = logging.getLogger(__name__)
+
+class AccountMoveLine(models.Model):
+    _inherit = 'account.move.line'
+
+    analytic_distribution_area = fields.Json(
+        inverse="_inverse_analytic_distribution",
+    ) 
+
+    analytic_distribution_activity = fields.Json(
+        inverse="_inverse_analytic_distribution",
+    )
