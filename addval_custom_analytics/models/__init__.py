6a511874c7891082d9439a7154c9e5d1bb9cf507
equirogag <equiroga@addval.cl>
2023-10-26 13:56:00 -0300

    only lines and task

--- addval_custom_analytics/models/__init__.py
+++ addval_custom_analytics/models/__init__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 
-from . import account_move_line, sale_order, project_task, sale_order_line
+from . import account_move_line, sale_order_line, project_task, purchase_order_line
