c68524dba7e5af21b7890bf79fcf987408b58bf9
equirogag <equiroga@addval.cl>
2023-10-31 17:24:53 -0300

    adjustement to domain task analytic

--- addval_custom_analytics/models/project_task.py
+++ addval_custom_analytics/models/project_task.py
@@ -8,7 +8,9 @@ class ProjectTask(models.Model):
     _inherit = 'project.task'
 
     def _get_area_domain(self):
-        return [("plan_id", "=", self.company_id.area_analytic_plan_id.id)]
+        company = self.env['res.company'].sudo.search(['id', '=', self.company_id.id])
+        company_plan =  company.area_analytic_plan_id
+        return [("plan_id", "=", company_plan.id)]
     
     area_analytic_account_id = fields.Many2one(
         comodel_name='account.analytic.account',
