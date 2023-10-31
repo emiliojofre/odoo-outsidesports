b2ada32608aa6c14371fcb3957ae2d5c6ea5a9e3
equirogag <equiroga@addval.cl>
2023-10-31 11:02:19 -0300

    update task domain

--- addval_custom_analytics/models/project_task.py
+++ addval_custom_analytics/models/project_task.py
@@ -7,14 +7,21 @@ _logger = logging.getLogger(__name__)
 class ProjectTask(models.Model):
     _inherit = 'project.task'
 
+    def _get_area_domain(self):
+        return [("plan_id", "=", self.env.ref("company_id.area_analytic_plan_id").id)]
+    
     area_analytic_account_id = fields.Many2one(
         comodel_name='account.analytic.account',
         string="Área",
         copy=False, check_company=True,  # Unrequired company
-        domain="['|', ('company_id', '=', False), ('company_id', '=', company_id), ('plan_id', '=', company_id.area_analytic_plan_id.id)]")
+        domain=_get_area_domain)
+    
+    def _get_activity_domain(self):
+        return [("plan_id", "=", self.env.ref("company_id.activity_analytic_plan_id").id)]
     
     activity_analytic_account_id = fields.Many2one(
         comodel_name='account.analytic.account',
         string="Actividad",
         copy=False, check_company=True,  # Unrequired company
-        domain="['|', ('company_id', '=', False), ('company_id', '=', company_id), ('plan_id', '=', company_id.activity_analytic_plan_id.id)]")
+        domain=_get_activity_domain)
+    
