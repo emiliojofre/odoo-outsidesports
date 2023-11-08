5f4055e2eddc4314ce5d4a3f7b7c4c06c48fbcdb
equirogag <equiroga@addval.cl>
2023-11-07 22:51:41 -0300

    update product_pricelist_items methods

--- addval_outside_custom/models/product_pricelist_item.py
+++ addval_outside_custom/models/product_pricelist_item.py
@@ -12,116 +12,39 @@ import logging
 
 _logger = logging.getLogger(__name__)
 
-class PricelistItem(models.Model):
+class ProductPricelistItem(models.Model):
     _inherit = "product.pricelist.item"
 
     applied_on = fields.Selection(
-        selection=[
-            ('3_global', "All Products"),
-            ('2_product_category', "Product Category"),
-            ('4_brand', "Marca"),
-            ('1_product', "Product"),
-            ('0_product_variant', "Product Variant"),
-        ],
-        string="Apply On",
-        default='3_global',
-        required=True,
-        help="Pricelist Item applicable on selected option")
+        selection_add=[
+            ('4_brand', "Marca")
+        ])
     
-    brand_id = fields.Many2one('wk.product.brand', string="Marca")
+    brand_id = fields.Many2one(
+        comodel_name='wk.product.brand', 
+        string="Marca", 
+        ondelete='cascade',
+        store=True)
 
-    @api.depends('applied_on', 'categ_id', 'product_tmpl_id', 'product_id', 'compute_price', 'fixed_price', \
+    @api.depends('applied_on', 'categ_id', 'product_tmpl_id', 'product_id', 'brand_id','compute_price', 'fixed_price', \
         'pricelist_id', 'percent_price', 'price_discount', 'price_surcharge')
     def _compute_name_and_price(self):
+        super(ProductPricelistItem, self)._compute_name_and_price()
         for item in self:
-            if item.categ_id and item.applied_on == '2_product_category':
-                item.name = _("Category: %s") % (item.categ_id.display_name)
-            elif item.brand_id and item.applied_on == '4_brand':
+            if item.brand_id and item.applied_on == '4_brand':
                 item.name = _("Marca: %s") % (item.brand_id.name)
-            elif item.product_tmpl_id and item.applied_on == '1_product':
-                item.name = _("Product: %s") % (item.product_tmpl_id.display_name)
-            elif item.product_id and item.applied_on == '0_product_variant':
-                item.name = _("Variant: %s") % (item.product_id.with_context(display_default_code=False).display_name)
-            else:
-                item.name = _("All Products")
-
-            if item.compute_price == 'fixed':
-                item.price = formatLang(
-                    item.env, item.fixed_price, monetary=True, dp="Product Price", currency_obj=item.currency_id)
-            elif item.compute_price == 'percentage':
-                item.price = _("%s %% discount", item.percent_price)
-            else:
-                item.price = _("%(percentage)s %% discount and %(price)s surcharge", percentage=item.price_discount, price=item.price_surcharge)
 
     @api.constrains('product_id', 'product_tmpl_id', 'categ_id')
     def _check_product_consistency(self):
+        super(ProductPricelistItem, self)._check_product_consistency()
         for item in self:
-            if item.applied_on == "2_product_category" and not item.categ_id:
-                raise ValidationError(_("Please specify the category for which this rule should be applied"))
-            elif item.applied_on == "4_brand" and not item.brand_id:
+            if item.applied_on == "4_brand" and not item.brand_id:
                 raise ValidationError(_("Please specify the brand for which this rule should be applied"))
-            elif item.applied_on == "1_product" and not item.product_tmpl_id:
-                raise ValidationError(_("Please specify the product for which this rule should be applied"))
-            elif item.applied_on == "0_product_variant" and not item.product_id:
-                raise ValidationError(_("Please specify the product variant for which this rule should be applied"))
-    
-    def _is_applicable_for(self, product, qty_in_product_uom):
-        """Check whether the current rule is valid for the given product & qty.
-
-        Note: self.ensure_one()
-
-        :param product: product record (product.product/product.template)
-        :param float qty_in_product_uom: quantity, expressed in product UoM
-        :returns: Whether rules is valid or not
-        :rtype: bool
-        """
+            
+    def _is_applicable_for(self, product, qty):
         self.ensure_one()
-        product.ensure_one()
-        res = True
-
-        is_product_template = product._name == 'product.template'
-        if self.min_quantity and qty_in_product_uom < self.min_quantity:
-            res = False
-        
-        elif self.brand_id:
-            _logger.warning('Entro if self.brand_id')
-            # Applied on a specific brand
-            bra = product.product_brand_id
-            _logger.warning('PRODUCTO:  %s', product)
-            _logger.warning('Marca: %s', bra)
-            if bra:
-                _logger.warning('entro al while')
-                _logger.warning('bra.id: %s', bra.id)
-                _logger.warning('self.brand_id.id: %s', self.brand_id.id)
-                if bra.id == self.brand_id.id:
-                    _logger.warning('Entro al if ')
-            if not bra:
-                res = False
-
-        elif self.categ_id:
-            # Applied on a specific category
-            cat = product.categ_id
-            while cat:
-                if cat.id == self.categ_id.id:
-                    break
-                cat = cat.parent_id
-            if not cat:
-                res = False
+        if self.applied_on == '4_brand':
+            return product.brand_id == self.brand_id
         else:
-            # Applied on a specific product template/variant
-            if is_product_template:
-                if self.product_tmpl_id and product.id != self.product_tmpl_id.id:
-                    res = False
-                elif self.product_id and not (
-                    product.product_variant_count == 1
-                    and product.product_variant_id.id == self.product_id.id
-                ):
-                    # product self acceptable on template if has only one variant
-                    res = False
-            else:
-                if self.product_tmpl_id and product.product_tmpl_id.id != self.product_tmpl_id.id:
-                    res = False
-                elif self.product_id and product.id != self.product_id.id:
-                    res = False
-
-        return res
+            return super(ProductPricelistItem, self)._compute_price(product, qty)
+    
