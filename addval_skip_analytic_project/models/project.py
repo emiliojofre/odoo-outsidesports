46e34829bc16e3bc54c1bb8866a9eb6166f19a8a
equirogag <equiroga@addval.cl>
2024-01-24 19:05:15 -0300

    test skip analytic account

--- addval_skip_analytic_project/models/project.py
+++ addval_skip_analytic_project/models/project.py
@@ -1,7 +1,13 @@
-from odoo import fields, models
+from odoo import fields, models, api
 
 class Project(models.Model):
     _inherit = "project.project"
 
+    @api.model
+    def _create_analytic_account_from_values(self, values):
+
+        return None
+
     def _create_analytic_account(self):
-        analytic_account = ''
+        for project in self:
+            project.write({'analytic_account_id': None})
