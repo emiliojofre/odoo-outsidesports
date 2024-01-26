7e64935f97d397e1cb71ea37250812cd6d2acd2c
equirogag <equiroga@addval.cl>
2024-01-26 12:40:11 -0300

    se hereda otra funcion para evitar la creación de cuentas

--- addval_skip_analytic_project/models/project.py
+++ addval_skip_analytic_project/models/project.py
@@ -5,9 +5,14 @@ class Project(models.Model):
 
     @api.model
     def _create_analytic_account_from_values(self, values):
-
         return None
 
     def _create_analytic_account(self):
         for project in self:
             project.write({'analytic_account_id': None})
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        for vals in vals_list:
+            vals['analytic_account_id'] = None
+        return super().create(vals_list)
