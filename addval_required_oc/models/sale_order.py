0308e92b9c7da4ef6f8acf77074514e39003a026
equirogag <equiroga@addval.cl>
2024-01-26 17:34:42 -0300

    comentar codigo

--- addval_required_oc/models/sale_order.py
+++ addval_required_oc/models/sale_order.py
@@ -25,10 +25,10 @@ class SaleOrder(models.Model):
         default='nothing'
     )
     
-    @api.model
-    def _cron_recurring_create_invoice(self):
-        if self.oc_hes_required == 'nothing':
-            return self._create_recurring_invoice(automatic=True)
+    # @api.model
+    # def _cron_recurring_create_invoice(self):
+    #     if self.oc_hes_required == 'nothing':
+    #         return self._create_recurring_invoice(automatic=True)
         # if self.oc_hes_required == 'oc':
         #     oc_actual = self.env['sale.reference'].search([('sale_id', '=', self.id), ('reference_doc_type_id.code', '=', '801')],limit=1)
         #     oc_in_invoice = False
@@ -38,26 +38,26 @@ class SaleOrder(models.Model):
 
         
 
-    def _create_invoices(self, grouped=False, final=False, date=None):
+    # def _create_invoices(self, grouped=False, final=False, date=None):
 
-        invoices = super()._create_invoices(grouped=grouped, final=final, date=date)
+    #     invoices = super()._create_invoices(grouped=grouped, final=final, date=date)
 
-        if self.oc_hes_required == 'nothing':
-            for invoice in invoices:
-                self.env['l10n_cl.account.invoice.reference'].create({
-                    'origin_doc_number': self.client_order_ref,
-                    'l10n_cl_reference_doc_type_id': order_oc.id,
-                    'reason': 'ORDEN DE COMPRA',
-                    'date': self.client_order_date,
-                    'move_id': invoice.id
-                })
+    #     if self.oc_hes_required == 'nothing':
+    #         for invoice in invoices:
+    #             self.env['l10n_cl.account.invoice.reference'].create({
+    #                 'origin_doc_number': self.client_order_ref,
+    #                 'l10n_cl_reference_doc_type_id': order_oc.id,
+    #                 'reason': 'ORDEN DE COMPRA',
+    #                 'date': self.client_order_date,
+    #                 'move_id': invoice.id
+    #             })
 
-                self.env['l10n_cl.account.invoice.reference'].create({
-                    'origin_doc_number': self.client_hes_ref,
-                    'l10n_cl_reference_doc_type_id': order_hes.id,
-                    'reason': 'HOJA DE ENTRADA DE SERVICIOS',
-                    'date': self.cliente_hes_date,
-                    'move_id': invoice.id
-                })
+    #             self.env['l10n_cl.account.invoice.reference'].create({
+    #                 'origin_doc_number': self.client_hes_ref,
+    #                 'l10n_cl_reference_doc_type_id': order_hes.id,
+    #                 'reason': 'HOJA DE ENTRADA DE SERVICIOS',
+    #                 'date': self.cliente_hes_date,
+    #                 'move_id': invoice.id
+    #             })
                 
-        return invoices
+    #     return invoices
