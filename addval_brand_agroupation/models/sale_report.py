9c7705839289b2e57a7d776d096657885f579819
edomaldonadoe <emaldonado@addval.cl>
2024-02-17 14:41:09 -0300

    test

--- addval_brand_agroupation/models/sale_report.py
+++ addval_brand_agroupation/models/sale_report.py
@@ -8,23 +8,16 @@ from odoo.exceptions import UserError
 class SaleReport(models.Model):
     _inherit = 'sale.report'
 
-    product_brand_id = fields.Many2one('wk.product.brand', related='product_tmpl_id.product_brand_id')
-
-    def _from_sale(self):
-        from_sale = super(SaleReport, self)._from_sale()
-        from_sale += """
-            LEFT JOIN wk_product_brand br ON br.id = t.product_brand_id
-            """
-        return from_sale
+    product_brand_id = fields.Many2one('wk.product.brand', readonly=True)
 
     def _select_sale(self):
         select_ = super(SaleReport, self)._select_sale()
         select_ += """,
-            br.id AS product_brand_id"""
+            t.product_brand_id AS product_brand_id"""
         return select_
 
     def _group_by_sale(self):
         group_by = super(SaleReport, self)._group_by_sale()
         group_by += """,
-            br.id"""
+            t.product_brand_id"""
         return group_by
