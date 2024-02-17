08ba7d9c82653414b30ef6dfc767102ca852daa4
edomaldonadoe <emaldonado@addval.cl>
2024-02-17 15:06:32 -0300

    test

--- addval_brand_agroupation/models/sale_report.py
+++ addval_brand_agroupation/models/sale_report.py
@@ -11,14 +11,15 @@ class SaleReport(models.Model):
     
     def _select_sale(self):
         select_ = super(SaleReport, self)._select_sale()
-        # Inserta tu línea justo antes de la cláusula AND
         select_ = select_.replace("p.product_tmpl_id,", "p.product_tmpl_id, t.product_brand_id AS product_brand_id,")
+        _logger.info("### SELECT ###")
+        _logger.info(select_)
         return select_
 
     def _group_by_sale(self):
         group_by = super(SaleReport, self)._group_by_sale()
-        _logger.info("### GROUP BY ###")
-        _logger.info(group_by)
         group_by += """,
             t.product_brand_id"""
+        _logger.info("### GROUP BY ###")
+        _logger.info(group_by)
         return group_by
