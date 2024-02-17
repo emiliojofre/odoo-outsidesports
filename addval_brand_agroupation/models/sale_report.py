ccdf4676ba161ffc925b49983b6b9cf9bb73ac2b
edomaldonadoe <emaldonado@addval.cl>
2024-02-17 14:22:46 -0300

    test

--- addval_brand_agroupation/models/sale_report.py
+++ addval_brand_agroupation/models/sale_report.py
@@ -10,116 +10,14 @@ class SaleReport(models.Model):
 
     product_brand_id = fields.Many2one('wk.product.brand', related='product_tmpl_id.product_brand_id')
 
-    def _from_sale(self):
-        return """
-            sale_order_line l
-            LEFT JOIN sale_order s ON s.id=l.order_id
-            JOIN res_partner partner ON s.partner_id = partner.id
-            LEFT JOIN product_product p ON l.product_id=p.id
-            LEFT JOIN product_template t ON p.product_tmpl_id=t.id
-            LEFT JOIN uom_uom u ON u.id=l.product_uom
-            LEFT JOIN uom_uom u2 ON u2.id=t.uom_id
-            LEFT JOIN product_template pt ON pt.id = p.product_tmpl_id
-            LEFT JOIN wk_product_brand wpb ON wpb.id = pt.product_brand_id
-            JOIN {currency_table} ON currency_table.company_id = s.company_id
-            """.format(
-            currency_table=self.env['res.currency']._get_query_currency_table(
-                {
-                    'multi_company': True,
-                    'date': {'date_to': fields.Date.today()}
-                }),
-            )
-    
     def _select_sale(self):
-        select_ = f"""
-            MIN(l.id) AS id,
-            l.product_id AS product_id,
-            t.uom_id AS product_uom,
-            CASE WHEN l.product_id IS NOT NULL THEN SUM(l.product_uom_qty / u.factor * u2.factor) ELSE 0 END AS product_uom_qty,
-            CASE WHEN l.product_id IS NOT NULL THEN SUM(l.qty_delivered / u.factor * u2.factor) ELSE 0 END AS qty_delivered,
-            CASE WHEN l.product_id IS NOT NULL THEN SUM((l.product_uom_qty - l.qty_delivered) / u.factor * u2.factor) ELSE 0 END AS qty_to_deliver,
-            CASE WHEN l.product_id IS NOT NULL THEN SUM(l.qty_invoiced / u.factor * u2.factor) ELSE 0 END AS qty_invoiced,
-            CASE WHEN l.product_id IS NOT NULL THEN SUM(l.qty_to_invoice / u.factor * u2.factor) ELSE 0 END AS qty_to_invoice,
-            CASE WHEN l.product_id IS NOT NULL THEN SUM(l.price_total
-                / {self._case_value_or_one('s.currency_rate')}
-                * {self._case_value_or_one('currency_table.rate')}
-                ) ELSE 0
-            END AS price_total,
-            CASE WHEN l.product_id IS NOT NULL THEN SUM(l.price_subtotal
-                / {self._case_value_or_one('s.currency_rate')}
-                * {self._case_value_or_one('currency_table.rate')}
-                ) ELSE 0
-            END AS price_subtotal,
-            CASE WHEN l.product_id IS NOT NULL THEN SUM(l.untaxed_amount_to_invoice
-                / {self._case_value_or_one('s.currency_rate')}
-                * {self._case_value_or_one('currency_table.rate')}
-                ) ELSE 0
-            END AS untaxed_amount_to_invoice,
-            CASE WHEN l.product_id IS NOT NULL THEN SUM(l.untaxed_amount_invoiced
-                / {self._case_value_or_one('s.currency_rate')}
-                * {self._case_value_or_one('currency_table.rate')}
-                ) ELSE 0
-            END AS untaxed_amount_invoiced,
-            COUNT(*) AS nbr,
-            s.name AS name,
-            s.date_order AS date,
-            s.state AS state,
-            s.partner_id AS partner_id,
-            s.user_id AS user_id,
-            s.company_id AS company_id,
-            s.campaign_id AS campaign_id,
-            s.medium_id AS medium_id,
-            s.source_id AS source_id,
-            t.categ_id AS categ_id,
-            s.pricelist_id AS pricelist_id,
-            s.analytic_account_id AS analytic_account_id,
-            s.team_id AS team_id,
-            p.product_tmpl_id,
-            partner.country_id AS country_id,
-            partner.industry_id AS industry_id,
-            partner.commercial_partner_id AS commercial_partner_id,
-            CASE WHEN l.product_id IS NOT NULL THEN SUM(p.weight * l.product_uom_qty / u.factor * u2.factor) ELSE 0 END AS weight,
-            CASE WHEN l.product_id IS NOT NULL THEN SUM(p.volume * l.product_uom_qty / u.factor * u2.factor) ELSE 0 END AS volume,
-            l.discount AS discount,
-            CASE WHEN l.product_id IS NOT NULL THEN SUM(l.price_unit * l.product_uom_qty * l.discount / 100.0
-                / {self._case_value_or_one('s.currency_rate')}
-                * {self._case_value_or_one('currency_table.rate')}
-                ) ELSE 0
-            END AS discount_amount,
-            s.id AS order_id,
-            pt.product_brand_id AS product_brand_id"""
-
-        additional_fields_info = self._select_additional_fields()
-        template = """,
-            %s AS %s"""
-        for fname, query_info in additional_fields_info.items():
-            select_ += template % (query_info, fname)
-
+        select_ = super(SaleReport, self)._select_sale()
+        select_ += """,
+            t.product_brand_id AS product_brand_id"""
         return select_
 
     def _group_by_sale(self):
-        return """
-            l.product_id,
-            l.order_id,
-            t.uom_id,
-            t.categ_id,
-            s.name,
-            s.date_order,
-            s.partner_id,
-            s.user_id,
-            s.state,
-            s.company_id,
-            s.campaign_id,
-            s.medium_id,
-            s.source_id,
-            s.pricelist_id,
-            s.analytic_account_id,
-            s.team_id,
-            p.product_tmpl_id,
-            partner.country_id,
-            partner.industry_id,
-            partner.commercial_partner_id,
-            l.discount,
-            s.id,
-            currency_table.rate,
-            pt.product_brand_id"""
+        group_by = super(SaleReport, self)._group_by_sale()
+        group_by += """,
+            t.product_brand_id"""
+        return group_by
