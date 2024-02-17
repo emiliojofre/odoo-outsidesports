5c5ac63b1980a089f09c73fcebae423f162696f9
edomaldonadoe <emaldonado@addval.cl>
2024-02-17 14:33:12 -0300

    test

--- addval_brand_agroupation/models/sale_report.py
+++ addval_brand_agroupation/models/sale_report.py
@@ -11,33 +11,20 @@ class SaleReport(models.Model):
     product_brand_id = fields.Many2one('wk.product.brand', related='product_tmpl_id.product_brand_id')
 
     def _from_sale(self):
-        from_ = """
-            sale_order_line l
-            LEFT JOIN sale_order s ON s.id=l.order_id
-            JOIN res_partner partner ON s.partner_id = partner.id
-            LEFT JOIN product_product p ON l.product_id=p.id
-            LEFT JOIN product_template t ON p.product_tmpl_id=t.id
-            LEFT JOIN uom_uom u ON u.id=l.product_uom
-            LEFT JOIN uom_uom u2 ON u2.id=t.uom_id
+        from_sale = super(SaleReport, self)._from_sale()
+        from_sale += """
             LEFT JOIN wk_product_brand br ON br.id = t.product_brand_id
-            JOIN {currency_table} ON currency_table.company_id = s.company_id
-            """.format(
-            currency_table=self.env['res.currency']._get_query_currency_table(
-                {
-                    'multi_company': True,
-                    'date': {'date_to': fields.Date.today()}
-                }),
-            )
-        return from_
+            """
+        return from_sale
 
     def _select_sale(self):
         select_ = super(SaleReport, self)._select_sale()
         select_ += """,
-            br.product_brand_id AS product_brand_id"""
+            br.id AS product_brand_id"""
         return select_
 
     def _group_by_sale(self):
         group_by = super(SaleReport, self)._group_by_sale()
         group_by += """,
-            br.product_brand_id"""
+            br.id"""
         return group_by
