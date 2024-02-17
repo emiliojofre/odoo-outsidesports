dc9f7307088ff307e493916fd9269cd37e138dfa
edomaldonadoe <emaldonado@addval.cl>
2024-02-17 15:02:36 -0300

    test

--- addval_brand_agroupation/models/sale_report.py
+++ addval_brand_agroupation/models/sale_report.py
@@ -8,19 +8,17 @@ class SaleReport(models.Model):
     _inherit = 'sale.report'
 
     product_brand_id = fields.Many2one('wk.product.brand', readonly=True)
-
+    
     def _select_sale(self):
         select_ = super(SaleReport, self)._select_sale()
-        _logger.info("### SELECT ###")
-        _logger.info(select_)
-        # select_ += """,
-        #     t.product_brand_id AS product_brand_id"""
+        # Inserta tu línea justo antes de la cláusula AND
+        select_ = select_.replace("p.product_tmpl_id,", "p.product_tmpl_id, t.product_brand_id AS product_brand_id")
         return select_
 
     def _group_by_sale(self):
         group_by = super(SaleReport, self)._group_by_sale()
         _logger.info("### GROUP BY ###")
         _logger.info(group_by)
-        # group_by += """,
-        #     t.product_brand_id"""
+        group_by += """,
+            t.product_brand_id"""
         return group_by
