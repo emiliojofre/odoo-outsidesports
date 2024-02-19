3b1a25277204b40e8ecefabb59be1ac5dfa0c328
edomaldonadoe <emaldonado@addval.cl>
2024-02-19 13:27:01 -0300

    pruebas

--- addval_website_sale/models/sale_order.py
+++ addval_website_sale/models/sale_order.py
@@ -8,6 +8,9 @@ from odoo.http import request
 from odoo.osv import expression
 from odoo.tools import float_is_zero
 
+import logging
+_logger = logging.getLogger(__name__)
+
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
@@ -24,7 +27,19 @@ class SaleOrder(models.Model):
     
     def _prepare_order_line_update_values(self, order_line, quantity, linked_line_id=False, **kwargs):
         values = super(SaleOrder, self)._prepare_order_line_update_values(order_line, quantity, linked_line_id=linked_line_id, **kwargs)
+        _logger.info("### _prepare_order_line_update_values ###")
+        _logger.info(values)
         
         values['analytic_distribution'] = {self.analytic_account_id.id: 100}
+        _logger.info(values)
 
+        return values
+    
+    def _prepare_order_line_values(self, product_id, quantity, linked_line_id=False,
+        no_variant_attribute_values=None, product_custom_attribute_values=None, **kwargs):
+        values = super(SaleOrder, self)._prepare_order_line_update_values(product_id, quantity, linked_line_id=linked_line_id,
+            no_variant_attribute_values=no_variant_attribute_values,
+            product_custom_attribute_values=product_custom_attribute_values, **kwargs)
+        _logger.info("### _prepare_order_line_values ###")
+        _logger.info(values)
         return values
