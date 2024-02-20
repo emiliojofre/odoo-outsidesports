14b0742bda02c653efb3234ad2ee2dcda1e18e42
edomaldonadoe <emaldonado@addval.cl>
2024-02-20 11:51:19 -0300

    restore

--- addval_website_sale/models/settings.py
+++ addval_website_sale/models/settings.py
@@ -4,9 +4,9 @@ from odoo import models, fields, api
 class ResConfigSettings(models.TransientModel):
     _inherit = "res.config.settings"
 
-    website_analytic_id = fields.Many2one(
+    api_analytic_id = fields.Many2one(
         'account.analytic.account',
-        related='company_id.website_analytic_id',
+        related='company_id.api_analytic_id',
         string = 'Cuenta analitica Website',
         store = True,
         readonly=False,
