af476636ee2626148cae04d54352a1ca66f152ea
edomaldonadoe <emaldonado@addval.cl>
2024-02-23 12:49:26 -0300

    test

---
+++ addval_cl_communes/controllers/main.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+
+from datetime import datetime
+from odoo import http, tools
+from odoo.fields import Command
+from odoo.http import request
+from odoo.addons.website_sale.controllers.main import WebsiteSale
+from odoo.exceptions import AccessError, MissingError, ValidationError
+import logging
+from odoo.tools.translate import _
+_logger = logging.getLogger(__name__)
+
+
+class WebsiteSaleAddressInfo(WebsiteSale):
+
+    @http.route(['/shop/address'], type='http', methods=['GET', 'POST'], auth="public", website=True, sitemap=False)
+    def address(self, **kw):
+        Partner = request.env['res.partner'].with_context(show_address=1).sudo()
+        order = request.website.sale_get_order()
+
+        redirection = self.checkout_redirection(order)
+        if redirection:
+            return redirection
+
+        mode = (False, False)
+        can_edit_vat = False
+        values, errors = {}, {}
+
+        partner_id = int(kw.get('partner_id', -1))
+
+        # IF PUBLIC ORDER
+        if order.partner_id.id == request.website.user_id.sudo().partner_id.id:
+            mode = ('new', 'billing')
+            can_edit_vat = True
+        # IF ORDER LINKED TO A PARTNER
+        else:
+            if partner_id > 0:
+                if partner_id == order.partner_id.id:
+                    mode = ('edit', 'billing')
+                    can_edit_vat = order.partner_id.can_edit_vat()
+                else:
+                    shippings = Partner.search([('id', 'child_of', order.partner_id.commercial_partner_id.ids)])
+                    if order.partner_id.commercial_partner_id.id == partner_id:
+                        mode = ('new', 'shipping')
+                        partner_id = -1
+                    elif partner_id in shippings.mapped('id'):
+                        mode = ('edit', 'shipping')
+                    else:
+                        return Forbidden()
+                if mode and partner_id != -1:
+                    values = Partner.browse(partner_id)
+            elif partner_id == -1:
+                mode = ('new', 'shipping')
+            else: # no mode - refresh without post?
+                return request.redirect('/shop/checkout')
+
+        # IF POSTED
+        if 'submitted' in kw and request.httprequest.method == "POST":
+            pre_values = self.values_preprocess(kw)
+            errors, error_msg = self.checkout_form_validate(mode, kw, pre_values)
+            post, errors, error_msg = self.values_postprocess(order, mode, pre_values, errors, error_msg)
+
+            if errors:
+                errors['error_message'] = error_msg
+                values = kw
+            else:
+                partner_id = self._checkout_form_save(mode, post, kw)
+                # We need to validate _checkout_form_save return, because when partner_id not in shippings
+                # it returns Forbidden() instead the partner_id
+                if isinstance(partner_id, Forbidden):
+                    return partner_id
+                fpos_before = order.fiscal_position_id
+                if mode[1] == 'billing':
+                    order.partner_id = partner_id
+                    # This is the *only* thing that the front end user will see/edit anyway when choosing billing address
+                    order.partner_invoice_id = partner_id
+                    if not kw.get('use_same'):
+                        kw['callback'] = kw.get('callback') or \
+                            (not order.only_services and (mode[0] == 'edit' and '/shop/checkout' or '/shop/address'))
+                    # We need to update the pricelist(by the one selected by the customer), because onchange_partner reset it
+                    # We only need to update the pricelist when it is not redirected to /confirm_order
+                    if kw.get('callback', False) != '/shop/confirm_order':
+                        request.website.sale_get_order(update_pricelist=True)
+                elif mode[1] == 'shipping':
+                    order.partner_shipping_id = partner_id
+
+                if order.fiscal_position_id != fpos_before:
+                    order._recompute_taxes()
+
+                # TDE FIXME: don't ever do this
+                # -> TDE: you are the guy that did what we should never do in commit e6f038a
+                order.message_partner_ids = [(4, partner_id), (3, request.website.partner_id.id)]
+                if not errors:
+                    return request.redirect(kw.get('callback') or '/shop/confirm_order')
+        
+        _logger.info("#### WEBSITE ADDRESS INFO ###")
+        _logger.info(values)
+
+        render_values = {
+            'website_sale_order': order,
+            'partner_id': partner_id,
+            'mode': mode,
+            'checkout': values,
+            'can_edit_vat': can_edit_vat,
+            'error': errors,
+            'callback': kw.get('callback'),
+            'only_services': order and order.only_services,
+            'account_on_checkout': request.website.account_on_checkout,
+            'is_public_user': request.website.is_public_user()
+        }
+        render_values.update(self._get_country_related_render_values(kw, render_values))
+        return request.render("website_sale.address", render_values)
