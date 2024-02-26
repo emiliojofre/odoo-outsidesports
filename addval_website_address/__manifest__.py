84045ac412d98b06a2cb7c99205d6dc3e8c58ac3
edomaldonadoe <emaldonado@addval.cl>
2024-02-26 12:54:43 -0300

    Cambio y separación de modulos de comunas

--- addval_website_address/__manifest__.py
+++ addval_website_address/__manifest__.py
@@ -1,28 +1,28 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Comunas por Región",
+    'name': "Modificaciones Formulario Direcciones Website",
 
     'summary': """
-        Comunas con dominio en regiones (addval_cl_communes)""",
+        Modificaciones Formulario Direcciones Website (addval_website_address)""",
 
     'description': """
-        Comunas con dominio en regiones (addval_cl_communes)
+        Modificaciones Formulario Direcciones Website (addval_website_address)
     """,
 
     "author": "Addval Connect",
     "website": "http://www.addval.cl",
-    "category": "Partner",
+    "category": "Website",
     "license": "Other proprietary",
     'version': '0.1',
     
     'data': [
-        'data/res_city.xml',
-        'views/res_partner.xml',
+        'views/website_sale_address.xml',
     ],
     
     'depends': [
         'base', 
-        'base_address_extended'
+        'website_sale',
+        'addval_cl_communes'
     ],
     
     'installable': True,
