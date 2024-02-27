0e46b91e4c831432a6f8746124a83794864d5320
edomaldonadoe <emaldonado@addval.cl>
2024-02-27 17:36:06 -0300

    test

--- addval_website_address/controllers/main.py
+++ addval_website_address/controllers/main.py
@@ -7,9 +7,9 @@ from datetime import datetime
 from werkzeug.exceptions import Forbidden
 
 from odoo import http, _
-from odoo.http import request
+from odoo.http import request, route
 from odoo.addons.website_sale.controllers.main import WebsiteSale
-
+from odoo.addons.portal.controllers.portal import CustomerPortal
 
 _logger = logging.getLogger(__name__)
 
@@ -115,3 +115,51 @@ class WebsiteSaleAddressInfo(WebsiteSale):
 
         render_values['cities'] = cities
         return request.render("website_sale.address", render_values)
+    
+
+class PortalAddressInfo(CustomerPortal):
+    @route(['/my/account'], type='http', auth='user', website=True)
+    def account(self, redirect=None, **post):
+        values = self._prepare_portal_layout_values()
+        partner = request.env.user.partner_id
+        values.update({
+            'error': {},
+            'error_message': [],
+        })
+
+        if post and request.httprequest.method == 'POST':
+            error, error_message = self.details_form_validate(post)
+            values.update({'error': error, 'error_message': error_message})
+            values.update(post)
+            if not error:
+                values = {key: post[key] for key in self.MANDATORY_BILLING_FIELDS}
+                values.update({key: post[key] for key in self.OPTIONAL_BILLING_FIELDS if key in post})
+                for field in set(['country_id', 'state_id']) & set(values.keys()):
+                    try:
+                        values[field] = int(values[field])
+                    except:
+                        values[field] = False
+                values.update({'zip': values.pop('zipcode', '')})
+                self.on_account_update(values, partner)
+                partner.sudo().write(values)
+                if redirect:
+                    return request.redirect(redirect)
+                return request.redirect('/my/home')
+
+        countries = request.env['res.country'].sudo().search([])
+        states = request.env['res.country.state'].sudo().search([])
+
+        values.update({
+            'partner': partner,
+            'countries': countries,
+            'states': states,
+            'has_check_vat': hasattr(request.env['res.partner'], 'check_vat'),
+            'partner_can_edit_vat': partner.can_edit_vat(),
+            'redirect': redirect,
+            'page_name': 'my_details',
+        })
+
+        response = request.render("portal.portal_my_details", values)
+        response.headers['X-Frame-Options'] = 'SAMEORIGIN'
+        response.headers['Content-Security-Policy'] = "frame-ancestors 'self'"
+        return response
