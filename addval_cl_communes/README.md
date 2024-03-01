03985fb9bb718392ab6037b15d642f2cfdffd585
equirogag <equiroga@addval.cl>
2024-03-01 16:42:47 -0300

    addval cl communes

---
+++ addval_cl_communes/README.md
@@ -0,0 +1,11 @@
+================
+Comunas y Regiones de Chile v16
+================
+
+Agrega las comunas y regiones chilenas
+
+Dependencias
+==============
+
+* base
+* base_address_extended
---
+++ addval_cl_communes/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Comunas por Región",
+
+    'summary': """
+        Comunas con dominio en regiones (addval_cl_communes)""",
+
+    'description': """
+        Comunas con dominio en regiones (addval_cl_communes)
+    """,
+
+    "author": "Addval Connect",
+    "website": "http://www.addval.cl",
+    "category": "Partner",
+    "license": "Other proprietary",
+    'version': '0.1',
+    
+    'data': [
+        'data/res_city.xml',
+        'views/res_partner.xml',
+    ],
+    
+    'depends': [
+        'base', 
+        'base_address_extended'
+    ],
+    
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    
+}
