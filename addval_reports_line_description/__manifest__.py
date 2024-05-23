bbc9583d76c01ceaa52a81008d3a016056e7581b
edomaldonadoe <emaldonado@addval.cl>
2024-05-23 14:25:06 -0400

    new addval_report_line_description

---
+++ addval_reports_line_description/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Custom Report Modifications',
+    'version': '1.0',
+    'category': 'Custom',
+    'summary': 'Custom modifications for report templates',
+    'depends': ['account', 'purchase', 'sale'],
+    'data': [
+        'views/views.xml',
+    ],
+}
