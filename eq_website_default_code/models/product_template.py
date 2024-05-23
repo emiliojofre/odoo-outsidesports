1f2963b22e4add31edb7ad25a98afb0651e29ad7
edomaldonadoe <emaldonado@addval.cl>
2024-05-23 13:37:57 -0400

    revisar

--- eq_website_default_code/models/product_template.py
+++ eq_website_default_code/models/product_template.py
@@ -24,17 +24,17 @@ class ProductTemplate(models.Model):
         combination_info = super(ProductTemplate, self)._get_combination_info(
             combination=combination, product_id=product_id, add_qty=add_qty, pricelist=pricelist,
             parent_combination=parent_combination, only_template=only_template)
-        default_code = 'TESTING'
+        default_code = ''
         pvp = 0
         if combination_info.get('product_id'):
             product_id = self.env['product.product'].browse(combination_info['product_id'])
-            default_code = 'Ciao'
+            default_code = product_id.default_code
             pvp = product_id.product_product_pvp
             if pvp == 1:
                 pvp = product_id.product_tmpl_id.product_tmpl_pvp
         if not default_code and combination_info.get('product_template_id'):
             product_id = self.env['product.template'].browse(combination_info['product_template_id'])
-            default_code = 'Hola'
+            default_code = product_id.default_code
             pvp = product_id.product_tmpl_pvp
         combination_info['default_code'] = default_code
         combination_info['pvp'] = pvp
