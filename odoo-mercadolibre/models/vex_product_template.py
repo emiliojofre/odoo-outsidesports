4dcba7262da564edc8f86c56d69e921f253023f6
Jefferson <jgonzalezchaca@gmail.com>
2025-08-29 16:36:31 -0500

    refactor: incluir el campo 'meli_description' en la sincronización de datos del modelo ProductTemplate

--- odoo-mercadolibre/models/vex_product_template.py
+++ odoo-mercadolibre/models/vex_product_template.py
@@ -133,14 +133,14 @@ class ProductTemplate(models.Model):
                 rec.meli_title,
                 rec.meli_listing_type,
                 rec.meli_condition,
+                rec.meli_description,
                 rec.meli_category_vex,
                 rec.meli_buying_mode,
-                rec.meli_base_price,
+                rec.meli_available_quantity,
                 rec.meli_currency_id,
                 rec.meli_thumbnail,
-                rec.meli_description,
                 rec.meli_warranty_type,
-                rec.meli_warranty_time
+                rec.meli_warranty_time,
             ])
             # Validar imágenes secundarias (al menos una)
             imagenes_ok = bool(rec.meli_pictures_ids)
