16f051a4fac16368be677b236f5e64fa74845289
Jefferson <jgonzalezchaca@gmail.com>
2025-09-12 10:13:15 -0500

    fix: comentar línea de tipo logístico en la lógica de publicación

--- odoo-mercadolibre/wizards/vex_soluciones_publish_product.py
+++ odoo-mercadolibre/wizards/vex_soluciones_publish_product.py
@@ -430,7 +430,7 @@ class VexPublishProductWizard(models.TransientModel):
             "pictures": pictures,
             "attributes": attributes,
             "sale_terms": sale_terms,
-            "logistic_type": self.meli_logistic_type,
+            # "logistic_type": self.meli_logistic_type,
             "description": {
                 "plain_text": self.meli_description,
             }
