53a92fc9cc0151253cbbbc63595f1e3ec5b9ee15
nlhconsultores <110052575+nlhconsultores@users.noreply.github.com>
2026-06-24 19:55:40 -0400

    Add files via upload

--- addval_website_chile/models/product_template.py
+++ addval_website_chile/models/product_template.py
@@ -12,12 +12,13 @@ class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
     def _get_combination_info(self, combination=False, product_id=False,
-                               add_qty=1, pricelist=False, **kwargs):
+                               add_qty=1, pricelist=False, parent_combination=False, **kwargs):
         res = super()._get_combination_info(
             combination=combination,
             product_id=product_id,
             add_qty=add_qty,
             pricelist=pricelist,
+            parent_combination=parent_combination,
             **kwargs
         )
         pl = pricelist
