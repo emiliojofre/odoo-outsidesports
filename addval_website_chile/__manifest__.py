a2f0f867716024ae2e1b9106258dc5795603e71c
nlhconsultores <110052575+nlhconsultores@users.noreply.github.com>
2026-06-24 15:02:15 -0400

    Add files via upload

--- addval_website_chile/__manifest__.py
+++ addval_website_chile/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Addval Website Chile - RUT y Teléfono',
-    'version': '16.0.2.0.2',
+    'version': '16.0.3.0.0',
     'author': 'NLH Consultores SpA',
     'license': 'OPL-1',
     'category': 'Website/eCommerce',
@@ -9,14 +9,8 @@
     'depends': ['addval_website_address', 'website_sale', 'eq_website_default_code'],
     'data': [
         'views/website_sale_address.xml',
+        'views/assets.xml',
     ],
-    'assets': {
-        'web.assets_frontend': [
-            'addval_website_chile/static/src/js/checkout_chile.js',
-            'addval_website_chile/static/src/js/product_price_fix.js',
-            'addval_website_chile/static/src/css/price_fix.css',
-        ],
-    },
     'installable': True,
     'auto_install': False,
 }
